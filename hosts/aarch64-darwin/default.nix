30c9cc79a329405402104581835555cee5608208
ue-sho <tssu45@gmail.com>
2025-04-03 23:53:42 +0900

    Add aarch64-darwin configurations and package definitions

---
+++ hosts/aarch64-darwin/default.nix
@@ -0,0 +1,47 @@
+{ inputs }:
+let
+  inherit (inputs) nix-darwin home-manager nixpkgs;
+
+  system = "aarch64-darwin";
+  username = "shohei.ueda";
+  pkgs = import nixpkgs {
+    inherit system;
+    config.allowUnfree = true;
+  };
+
+  # {{{ Homebrew casks
+  casks = [
+    "google-chrome"
+    "discord"
+    "visual-studio-code"
+    "docker"
+    "slack"
+    "iterm2"
+    "rectangle"
+    "aws-vault"
+    "bitwarden"
+    "font-jetbrains-mono-nerd-font"
+    "font-sauce-code-pro-nerd-font"
+    "google-japanese-ime"
+    "karabiner-elements"
+    "warp"
+    "wireshark"
+  ];
+  # }}}
+in
+nix-darwin.lib.darwinSystem {
+  modules = [
+    home-manager.darwinModules.home-manager
+    (import ../../modules/nix-darwin { inherit system username pkgs casks; })
+    {
+      home-manager.backupFileExtension = "bk.nix";
+      home-manager.users.${username} = {
+        imports = [
+          ../../modules/home-manager
+        ];
+
+        home.packages = import ./packages.nix { inherit pkgs; };
+      };
+    }
+  ];
+}
