46a56f45278fe431abb63f4bee9bdfae9a8c7554
ue-sho <tssu45@gmail.com>
2025-11-05 15:18:02 +0900

    Add .gitignore for mise configuration; clean up x86_64-darwin package list by removing unused programming languages and tools

--- hosts/x86_64-darwin/packages.nix
+++ hosts/x86_64-darwin/packages.nix
@@ -1,33 +1,6 @@
 { pkgs, ... }:
 with pkgs; [
   # Basic Tools installed by modules/home-manager/default.nix
-
-  # Programming Languages & SDKs
-  volta  # for node.js / pnpm
-  python312
-  kotlin
   jdk21
-  gradle
-  go
-
-  # Database Related
-  mysql80
-  postgresql_17
-  flyway
-
-  # Container Tools
-  kubectl
-  minikube
-  kubernetes-helm
-  kustomize
-  kubectx
-
-  # Additional Tools
-  terraform
-  graphviz
-  shellcheck
-  peco
   mise
-  libyaml # for ruby
-  gmp     # for ruby
 ]
